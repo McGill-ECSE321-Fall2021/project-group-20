54d50127a6c86d1116af261567328d933ba4f91c
AlexandruB30 <73802110+AlexandruB30@users.noreply.github.com>
2021-10-07 22:31:01 -0400

    Initial setup and commit of the LibrarySystem Backend using Spring

---
+++ LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/LibrarySystemApplication.java
@@ -0,0 +1,17 @@
+package ca.mcgill.ecse321.librarysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@SpringBootApplication
+public class LibrarySystemApplication {
+
+	@RequestMapping("/")
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarySystemApplication.class, args);
+	}
+
+}
