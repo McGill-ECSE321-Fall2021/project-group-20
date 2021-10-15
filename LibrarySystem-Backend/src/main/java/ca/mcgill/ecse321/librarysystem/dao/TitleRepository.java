55f1ff4fcb788f8831caa9391d810b3c438bf0eb
royalblaster <ehsanahmed99@gmail.com>
2021-10-15 17:46:36 -0400

    Update TitleRepository
    Put it back to what it was -> using proper foreign key

--- LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/dao/TitleRepository.java
+++ LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/dao/TitleRepository.java
@@ -9,10 +9,10 @@ import ca.mcgill.ecse321.librarysystem.model.Item;
 import ca.mcgill.ecse321.librarysystem.model.Title;
 
 public interface TitleRepository extends CrudRepository<Title, String>{
-	List<Title> findByAuthor(Author author);
-	List<Title> findByAuthorIn(List<Author> author);
-	List<Title> findByItem(Item item);
-	List<Title> findByItemIn(List<Item> item);
+	List<Title> findByAuthor(Author authorID);
+	List<Title> findByAuthorIn(List<Author> authorID);
+	List<Title> findByItem(Item itemBarcode);
+	List<Title> findByItemIn(List<Item> itemBarcode);
 	List<Title> findByName(String name);
 	List<Title> findByPubDate(String pubDate); 
 	List<Title> findByNameAndPubDate(String name, String pubDate);
