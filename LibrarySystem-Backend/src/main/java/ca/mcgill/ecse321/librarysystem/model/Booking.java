b2ad057bd25ded6c32cb0df2038fb2a9a90a3d42
HPatel999 <tornadepatel@gmail.com>
2021-10-15 17:24:15 -0400

    Mini-error fix
    Fix errors that came from a merge

--- LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/model/Booking.java
+++ LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/model/Booking.java
@@ -47,7 +47,6 @@ public class Booking
   //------------------------
   public Booking(){}
 
-  public Booking(){}
 
   public Booking(Date aStartDate, Date aEndDate, BookingType aType, Item aItembooked, User aUser)
   {
