bebd4b19282cea09b25055a8cee4061587244028
royalblaster <ehsanahmed99@gmail.com>
2021-10-16 11:58:19 -0400

    Test in finding titles with multiple authors
    I have created the template for all the tests for TestTitleRepositorPersistence.  I have also created the class for TestAuthorRepositorPersistence. I have also made a test and succeeded findByAuthorIn crud.

---
+++ LibrarySystem-Backend/src/test/java/ca/mcgill/ecse321/librarysystem/dao/TestAuthorRepositoryPersistence.java
@@ -0,0 +1,8 @@
+package ca.mcgill.ecse321.librarysystem.dao;
+
+public class TestAuthorRepositoryPersistence {
+	
+	
+	
+
+}
--- LibrarySystem-Backend/src/test/java/ca/mcgill/ecse321/librarysystem/dao/TestTitleRepositoryPersistence.java
+++ LibrarySystem-Backend/src/test/java/ca/mcgill/ecse321/librarysystem/dao/TestTitleRepositoryPersistence.java
@@ -1,8 +1,8 @@
 package ca.mcgill.ecse321.librarysystem.dao;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.AfterEach;
@@ -31,7 +31,7 @@ public class TestTitleRepositoryPersistence {
 	}
 	
 	@Test
-	public void testPersistAndLoadTitle() {
+	public void testPersistAndLoadTitleByAuthorID() {
 		//Check if a title is stored in the repository 
 		Author a1 = new Author("J.K.", "Rowling");
 		Author a2 = new Author("Dan", "Hosi");
@@ -40,23 +40,97 @@ public class TestTitleRepositoryPersistence {
 		t1.addAuthor(a2);
 		t1.addAuthor(a3);
 		
-		this.authorRepository.save(a1);
-		this.authorRepository.save(a2);
-		this.authorRepository.save(a3);
-		this.titleRepository.save(t1);
+		authorRepository.save(a1);
+		authorRepository.save(a2);
+		authorRepository.save(a3);
+		titleRepository.save(t1);
 
 		t1 = null;
 		
-		List<Title> listTitlesByAuthor = titleRepository.findByAuthor(a1);
+		List<Title> listTitlesByAuthorID = titleRepository.findByAuthor(a1);
 		
-		for (Title t : listTitlesByAuthor) {
+		for (Title t : listTitlesByAuthorID) {
 			assertEquals(a1.getAuthorID(), t.getAuthor(0).getAuthorID());
 			assertEquals(a2.getAuthorID(), t.getAuthor(1).getAuthorID());
 			assertEquals(a3.getAuthorID(), t.getAuthor(2).getAuthorID());
-			//if (t.getAuthor().get(0).getAuthorID().equals(a1.getAuthorID())) t1 = t;
 		}
+	}
+	
+	@Test 
+	public void testPersistAndLoadTitleByAuthorIDs() {
 		
-		//assertNotNull(t1);
-		//assertEquals(a1.getAuthorID(),t1.getAuthor(0).getAuthorID());
+		//Check if a title is stored in the repository 
+		Author a1 = new Author("J.K.", "Rowling");
+		Author a2 = new Author("Dan", "Hosi");
+		Author a3 = new Author("Harsh", "Patel");
+		Title t1 = new Title("Harry Potter and The Philosopher's Stone", "October 31th, 2021", a1);
+		t1.addAuthor(a2);
+		t1.addAuthor(a3);
+		authorRepository.save(a1);
+		authorRepository.save(a2);
+		authorRepository.save(a3);
+		titleRepository.save(t1);
+		
+		Author a4 = new Author("Walt", "Disney");
+		Title t2 = new Title("Mickey Mouse","", a4);
+		authorRepository.save(a4);
+		titleRepository.save(t2);
+		
+		Author a5 = new Author("Harry", "Leib");
+		Title t3 = new Title("Probability and statistics", "September 19th, 2021", a5);
+		authorRepository.save(a5);
+		titleRepository.save(t3);
+		
+		Author a6 = new Author("Ehsan", "Ahmed");
+		Title t4 = new Title("Guide to How to Screw Up in Life","October 22nd, 2021", a6);
+		t4.addAuthor(a2);
+		t4.addAuthor(a3);
+		authorRepository.save(a6);
+		titleRepository.save(t4);
+		
+		t1 = null;
+		t2 = null;
+		t3 = null;
+		t4 = null;
+		
+		List<Author> list_authors = new ArrayList<Author>(); 
+		list_authors.add(a2);
+		list_authors.add(a4);
+		
+		List<Title> listTitlesByAuthorIDs =  titleRepository.findByAuthorIn(list_authors);
+		
+		
+		assertEquals(a2.getAuthorID(), listTitlesByAuthorIDs.get(0).getAuthor(1).getAuthorID());
+		assertEquals(a4.getAuthorID(), listTitlesByAuthorIDs.get(1).getAuthor(0).getAuthorID());
+		assertEquals(a2.getAuthorID(), listTitlesByAuthorIDs.get(2).getAuthor(1).getAuthorID());		
+	}
+	
+	@Test 
+	public void testPersistAndLoadTitleByItemBarCode() {
+		
+//		Author a1 = new Author("J.K.", "Rowling");
+//		Author a2 = new Author("Dan", "Hosi");
+//		Author a3 = new Author("Harsh", "Patel");
+//		Title t1 = new Title("Harry Potter and The Philosopher's Stone", "October 31th, 2021", a1);
+		
+		
+		
+	}
+	
+	@Test 
+	public void testPersistAndLoadTitleByItemBarCodes() {
+	}
+	
+	
+	@Test 
+	public void testPersistAndLoadTitleName() {
+	}
+	
+	@Test 
+	public void testPersistAndLoadTitlePubDate() {
+	}
+	
+	@Test 
+	public void testPersistAndLoadTitleNameAndPubDate() {
 	}
 }
