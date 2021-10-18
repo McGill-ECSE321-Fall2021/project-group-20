3fa816690d4bfcf8461df2095de2d5cce474e411
royalblaster <ehsanahmed99@gmail.com>
2021-10-17 23:11:31 -0400

    Added Comments for TestTitleRepositoryPersistence and TestAuthorRepositoryPersistence
    Added comments to make the code understandable and clean.

--- LibrarySystem-Backend/src/test/java/ca/mcgill/ecse321/librarysystem/dao/TestAuthorRepositoryPersistence.java
+++ LibrarySystem-Backend/src/test/java/ca/mcgill/ecse321/librarysystem/dao/TestAuthorRepositoryPersistence.java
@@ -27,6 +27,7 @@ public class TestAuthorRepositoryPersistence {
 	
 	@AfterEach
 	public void clearDatabase() {
+		//clear database
 		titleRepository.deleteAll();
 		authorRepository.deleteAll();
 	}
@@ -34,83 +35,103 @@ public class TestAuthorRepositoryPersistence {
 	@Test
 	public void testPersistAndLoadAuthorByAuthorID() {
 		
-		Author a1 = new Author("J.K.", "Rowling");
-		Title t1 = new Title("Harry Potter and The Philosopher's Stone", "October 31th, 2021", a1);
-		Author a3 = new Author("Harsh", "Patel");
-		authorRepository.save(a1);
-		authorRepository.save(a3);
-		titleRepository.save(t1);
-		
-		String id = a1.getAuthorID();
+		//create data
+		Author author1 = new Author("J.K.", "Rowling");
+		Title title1 = new Title("Harry Potter and The Philosopher's Stone", "October 31th, 2021", author1);
+		Author author3 = new Author("Harsh", "Patel");
+		//store data
+		authorRepository.save(author1);
+		authorRepository.save(author3);
+		titleRepository.save(title1);
+		//preparing test
+		String id = author1.getAuthorID();
 
-		t1=null;
-		
-		Author a = authorRepository.findByAuthorID(id);
-		assertEquals(id, a.getAuthorID());
+		title1=null;
+		//test if author is stored properly by author id 
+		Author authorOutput = authorRepository.findByAuthorID(id);
+		assertEquals(id, authorOutput.getAuthorID());
 		
 	}
 	
 	@Test
 	public void testPersistAndLoadAuthorByFirstName() {
-		Author a1 = new Author("Ehsan", "Ahmed");
-		Title t1 = new Title("Merlin's beard", "October 22nd, 2021", a1);
-		Author a2 = new Author("Abdel", "Majid");
-		authorRepository.save(a1);
-		authorRepository.save(a2);
-		titleRepository.save(t1);
 		
+		//create data
+		Author author1 = new Author("Ehsan", "Ahmed");
+		Title title1 = new Title("Merlin's beard", "October 22nd, 2021", author1);
+		Author author2 = new Author("Abdel", "Majid");
+		
+		//save data
+		authorRepository.save(author1);
+		authorRepository.save(author2);
+		titleRepository.save(title1);
+		
+		//preparing test
 		String firstName = "Ehsan";
 		
 		String fakeFirstName = "Ferrie";
 
 		
-		
+		//test if data is saved by checking the author's first name and see if fake name is in database
 		List<Author> listAuthors = authorRepository.findByFirstName(firstName);
 		assertEquals(firstName, listAuthors.get(0).getFirstName());
 		
 		listAuthors = authorRepository.findByFirstName(fakeFirstName);
 		assertEquals(0, listAuthors.size());
 		
-		t1.delete();
+		//delete data
+		title1.delete();
 	}
 	 
 	@Test
 	public void testPersistAndLoadAuthorByLastName() {
-		Author a1 = new Author("Ehsan", "Ahmed");
-		Title t1 = new Title("Merlin's beard", "October 22nd, 2021", a1);
-		Author a2 = new Author("Abdel", "Majid");
-		authorRepository.save(a1);
-		authorRepository.save(a2);
-		titleRepository.save(t1);
+		//create data
+		Author author1 = new Author("Ehsan", "Ahmed");
+		Title title1 = new Title("Merlin's beard", "October 22nd, 2021", author1);
+		Author author2 = new Author("Abdel", "Majid");
 		
+		//save data
+		authorRepository.save(author1);
+		authorRepository.save(author2);
+		titleRepository.save(title1);
+		
+		//preparing test
 		String lastName = "Ahmed";
 		
 		String fakeLastName = "Lover";
 
 		
+		//test if data is saved by checking the author's last name and see if fake name is in database
 		List<Author> listAuthors = authorRepository.findByLastName(lastName);
 		assertEquals(lastName, listAuthors.get(0).getLastName());
-		
+		//test if data is saved properly by checking if fake name is not in database
 		listAuthors = authorRepository.findByFirstName(fakeLastName);
 		assertEquals(0, listAuthors.size());
 		
-		t1.delete();
+		//delete data
+		title1.delete();
 	}
 	
 	@Test
 	public void testPersistAndLoadAuthorByFirstNameAndLastName() {
-		Author a1 = new Author("Ehsan", "Ahmed");
-		Title t1 = new Title("Black Chaos", "October 22nd, 2021", a1);
-		Author a2 = new Author("Dan", "Hosi");
-		authorRepository.save(a1);
-		authorRepository.save(a2);
-		titleRepository.save(t1);
 		
+		//create data
+		Author author1 = new Author("Ehsan", "Ahmed");
+		Title title1 = new Title("Black Chaos", "October 22nd, 2021", author1);
+		Author author2 = new Author("Dan", "Hosi");
+		
+		//save data
+		authorRepository.save(author1);
+		authorRepository.save(author2);
+		titleRepository.save(title1);
+		
+		//preparing test
 		String firstName = "Ehsan";
 		String fakeFirstName = "Ferrie";
 		String lastName = "Ahmed";
 		String fakeLastName = "Lover";
 		
+		//test if data is saved by checking the author's first and last name and see if fake names are in database
 		List<Author> listAuthors = authorRepository.findByFirstNameAndLastName(firstName, lastName);
 		assertEquals(firstName, listAuthors.get(0).getFirstName());
 		assertEquals(lastName, listAuthors.get(0).getLastName());
@@ -118,69 +139,86 @@ public class TestAuthorRepositoryPersistence {
 		listAuthors = authorRepository.findByFirstNameAndLastName(fakeFirstName, fakeLastName);
 		assertEquals(0, listAuthors.size());
 		
-		t1.delete();
+		//delete data
+		title1.delete();
 	}
 	
 	@Test
 	public void testPersistAndLoadAuthorByTitles() {
-		//List<Author> findByTitlesIn(List<Title> titles);
-		Author a1 = new Author("Alexandru", "Bangala");
-		authorRepository.save(a1);
-		Title t1 = new Title("The Goat", "June 1rst, 2021", a1);
-		titleRepository.save(t1);
-		Title t2 = new Title("The Genius", "July 1rst, 2021", a1);
-		titleRepository.save(t2);
-		Author a2 = new Author("Harsh", "Patel");
-		authorRepository.save(a2);
-		Title t3 = new Title("The Crazy", "January 1rst, 2021", a2);
-		titleRepository.save(t3);
+		//create and save data;
+		Author author1 = new Author("Alexandru", "Bangala");
+		authorRepository.save(author1);
+		Title title1 = new Title("The Goat", "June 1rst, 2021", author1);
+		titleRepository.save(title1);
+		Title title2 = new Title("The Genius", "July 1rst, 2021", author1);
+		titleRepository.save(title2);
+		Author author2 = new Author("Harsh", "Patel");
+		authorRepository.save(author2);
+		Title title3 = new Title("The Crazy", "January 1rst, 2021", author2);
+		titleRepository.save(title3);
 		
+		//preparing test
 		List<Title> titles = new ArrayList<Title>();
 		
-		titles.add(t1);
-		titles.add(t3);
+		titles.add(title1);
+		titles.add(title3);
+		
+		//test if author(s) is/are stored from a list of titles
 		List<Author> listAuthors = authorRepository.findByTitlesIn(titles);
 		
+		//preparing test 
 		String firstNameFirstAuthor = "Alexandru";
 		String lastNameFirstAuthor = "Bangala";
 		
 		String firstNameSecondAuthor = "Harsh";
 		String lastNameSecondAuthor = "Patel";
 		
+		
 		assertEquals(firstNameFirstAuthor,listAuthors.get(0).getFirstName());
 		assertEquals(lastNameFirstAuthor,listAuthors.get(0).getLastName());
 		assertEquals(firstNameSecondAuthor,listAuthors.get(1).getFirstName());
 		assertEquals(lastNameSecondAuthor,listAuthors.get(1).getLastName());
 		
-		t1.delete();
-		t2.delete();
-		t3.delete();
+		//delete title data
+		title1.delete();
+		title2.delete();
+		title3.delete();
 	} 
 	
 	@Test
 	public void testPersistAndLoadAuthorAndSeeExistsByFirstName() {
+		
+		//create and save data
 		Author a1 = new Author("Ehsan", "Ahmed");
 		authorRepository.save(a1);
+		//preparing test
 		String firstName = "Ehsan";
+		//check if author exists in the database by first name
 		boolean checking = authorRepository.existsByFirstName(firstName);
 		assertTrue(checking);
 	}
 	 
 	@Test
 	public void testPersistAndLoadAuthorAndSeeExistsByLastName() {
+		//create and save data
 		Author a1 = new Author("Ehsan", "Ahmed");
 		authorRepository.save(a1);
+		//preparing test
 		String lastName = "Ahmed";
+		//check if author exists in the database by last name
 		boolean checking = authorRepository.existsByLastName(lastName);
 		assertTrue(checking);
 	}
 	
 	@Test
 	public void testPersistAndLoadAuthorAndSeeExistsByFirstNameAndLastName() {	
+		//create and save data
 		Author a1 = new Author("Ehsan", "Ahmed");
 		authorRepository.save(a1);
+		//preparing test
 		String firstName = "Ehsan";
 		String lastName = "Ahmed";
+		//check if author exists in the database by first and last name
 		boolean checking = authorRepository.existsByFirstNameAndLastName(firstName, lastName);
 		assertTrue(checking);
 	}
--- LibrarySystem-Backend/src/test/java/ca/mcgill/ecse321/librarysystem/dao/TestTitleRepositoryPersistence.java
+++ LibrarySystem-Backend/src/test/java/ca/mcgill/ecse321/librarysystem/dao/TestTitleRepositoryPersistence.java
@@ -40,6 +40,7 @@ public class TestTitleRepositoryPersistence {
 	
 	@AfterEach
 	public void clearDatabase() {
+		//clearing database
 		itemRepository.deleteAll();
 		titleRepository.deleteAll();
 		authorRepository.deleteAll();
@@ -50,111 +51,130 @@ public class TestTitleRepositoryPersistence {
 	
 	@Test
 	public void testPersistAndLoadTitleByTitleID() {
-		Author a1 = new Author("J.K.", "Rowling");
-		authorRepository.save(a1);
-		Title t1 = new Title("Harry Potter and The Philosopher's Stone", "October 31th, 2021", a1);
-		titleRepository.save(t1);
+		//create and save data
+		Author author1 = new Author("J.K.", "Rowling");
+		authorRepository.save(author1);
+		Title title1 = new Title("Harry Potter and The Philosopher's Stone", "October 31th, 2021", author1);
+		titleRepository.save(title1);
 		
-		Title PersistedTitle = titleRepository.findByTitleID(t1.getTitleID());
+		//testing whether title is stored properly by finding the title by its id
+		Title PersistedTitle = titleRepository.findByTitleID(title1.getTitleID());
 		
 		assertNotNull(PersistedTitle);
-		assertEquals(PersistedTitle.getTitleID(), t1.getTitleID());
+		assertEquals(PersistedTitle.getTitleID(), title1.getTitleID());
 	}
 	
 	@Test
 	public void testPersistAndLoadTitleByAuthorID() {
-
-		Author a1 = new Author("J.K.", "Rowling");
-		Author a2 = new Author("Dan", "Hosi");
-		Author a3 = new Author("Harsh", "Patel");
-		Title t1 = new Title("Harry Potter and The Philosopher's Stone", "October 31th, 2021", a1);
-		t1.addAuthor(a2);
-		t1.addAuthor(a3);
 		
-		authorRepository.save(a1);
-		authorRepository.save(a2);
-		authorRepository.save(a3);
-		titleRepository.save(t1);
-
-		t1 = null;
+		//Create data
+		Author author1 = new Author("J.K.", "Rowling");
+		Author author2 = new Author("Dan", "Hosi");
+		Author author3 = new Author("Harsh", "Patel");
+		Title title1 = new Title("Harry Potter and The Philosopher's Stone", "October 31th, 2021", author1);
+		title1.addAuthor(author2);
+		title1.addAuthor(author3);
 		
-		List<Title> listTitlesByAuthorID = titleRepository.findByAuthor(a1);
+		//Save data
+		authorRepository.save(author1);
+		authorRepository.save(author2);
+		authorRepository.save(author3);
+		titleRepository.save(title1);
 		
+		//Test if title is stored by checking its name 
+		title1 = null;
+		
+		List<Title> listTitlesByAuthorID = titleRepository.findByAuthor(author1);
+		
+		//testing whether title is stored properly by finding the title by its author id
 		for (Title t : listTitlesByAuthorID) {
-			assertEquals(a1.getAuthorID(), t.getAuthor(0).getAuthorID());
-			assertEquals(a2.getAuthorID(), t.getAuthor(1).getAuthorID());
-			assertEquals(a3.getAuthorID(), t.getAuthor(2).getAuthorID());
+			assertEquals(author1.getAuthorID(), t.getAuthor(0).getAuthorID());
+			assertEquals(author2.getAuthorID(), t.getAuthor(1).getAuthorID());
+			assertEquals(author3.getAuthorID(), t.getAuthor(2).getAuthorID());
 		}
 	}
 	
 	@Test 
 	public void testPersistAndLoadTitleByAuthorIDs() {
 		
-		Author a1 = new Author("J.K.", "Rowling");
-		Author a2 = new Author("Dan", "Hosi");
-		Author a3 = new Author("Harsh", "Patel");
-		Title t1 = new Title("Harry Potter and The Philosopher's Stone", "October 31th, 2021", a1);
-		t1.addAuthor(a2);
-		t1.addAuthor(a3);
-		authorRepository.save(a1);
-		authorRepository.save(a2);
-		authorRepository.save(a3);
-		titleRepository.save(t1);
+		//create data
+		Author author1 = new Author("J.K.", "Rowling");
+		Author author2 = new Author("Dan", "Hosi");
+		Author author3 = new Author("Harsh", "Patel");
+		Title title1 = new Title("Harry Potter and The Philosopher's Stone", "October 31th, 2021", author1);
+		title1.addAuthor(author2);
+		title1.addAuthor(author3);
 		
-		Author a4 = new Author("Walt", "Disney");
-		Title t2 = new Title("Mickey Mouse","", a4);
-		authorRepository.save(a4);
-		titleRepository.save(t2);
+		//save data
+		authorRepository.save(author1);
+		authorRepository.save(author2);
+		authorRepository.save(author3);
+		titleRepository.save(title1);
 		
-		Author a5 = new Author("Harry", "Leib");
-		Title t3 = new Title("Probability and statistics", "September 19th, 2021", a5);
-		authorRepository.save(a5);
-		titleRepository.save(t3);
+		//create and save other data
+		Author author4 = new Author("Walt", "Disney");
+		Title title2 = new Title("Mickey Mouse","", author4);
+		authorRepository.save(author4);
+		titleRepository.save(title2);
 		
-		Author a6 = new Author("Ehsan", "Ahmed");
-		Title t4 = new Title("Guide to How to Screw Up in Life","October 22nd, 2021", a6);
-		t4.addAuthor(a2);
-		t4.addAuthor(a3);
-		authorRepository.save(a6);
-		titleRepository.save(t4);
+		Author author5 = new Author("Harry", "Leib");
+		Title title3 = new Title("Probability and statistics", "September 19th, 2021", author5);
+		authorRepository.save(author5);
+		titleRepository.save(title3);
 		
-		t1 = null;
-		t2 = null;
-		t3 = null;
-		t4 = null;
+		Author author6 = new Author("Ehsan", "Ahmed");
+		Title title4 = new Title("Guide to How to Screw Up in Life","October 22nd, 2021", author6);
+		title4.addAuthor(author2);
+		title4.addAuthor(author3);
+		authorRepository.save(author6);
+		titleRepository.save(title4);
 		
+		//Test if title is stored by checking its name 
+		title1 = null;
+		title2 = null;
+		title3 = null;
+		title4 = null;
+		
+		
+		//Test to see if data is stored by looking through a list of authors
 		List<Author> list_authors = new ArrayList<Author>(); 
-		list_authors.add(a2);
-		list_authors.add(a4);
+		list_authors.add(author2);
+		list_authors.add(author4);
 		
 		List<Title> listTitlesByAuthorIDs =  titleRepository.findByAuthorIn(list_authors);
 		
 		
-		assertEquals(a2.getAuthorID(), listTitlesByAuthorIDs.get(0).getAuthor(1).getAuthorID());
-		assertEquals(a4.getAuthorID(), listTitlesByAuthorIDs.get(1).getAuthor(0).getAuthorID());
-		assertEquals(a2.getAuthorID(), listTitlesByAuthorIDs.get(2).getAuthor(1).getAuthorID());		
+		assertEquals(author2.getAuthorID(), listTitlesByAuthorIDs.get(0).getAuthor(1).getAuthorID());
+		assertEquals(author4.getAuthorID(), listTitlesByAuthorIDs.get(1).getAuthor(0).getAuthorID());
+		assertEquals(author2.getAuthorID(), listTitlesByAuthorIDs.get(2).getAuthor(1).getAuthorID());		
 	}
 	
 	@Test 
 	public void testPersistAndLoadTitleByItemBarCode() {	
+		
+		//create and save data
 		Address myadress = new Address("13","Elmstreet","Montreal","HeLL666","Quebec","Canada");
 		addressRepository.save(myadress);
-		Calendar c1 = new Calendar();
-		calendarRepository.save(c1);
-		LibrarySystem l1 = new LibrarySystem(myadress, c1);
-		librarySystemRepository.save(l1);
-		Author a1 = new Author("J.K.", "Rowling");
-		authorRepository.save(a1);
-		Title t1 = new Title("Hell Dragon King", "October 31th, 1999", a1);
-		titleRepository.save(t1);
-		Item myItem = new Item(Status.Available, l1, t1);
+		Calendar calendar1 = new Calendar();
+		calendarRepository.save(calendar1);
+		LibrarySystem library1 = new LibrarySystem(myadress, calendar1);
+		librarySystemRepository.save(library1);
+		Author author1 = new Author("J.K.", "Rowling");
+		authorRepository.save(author1);
+		Title title1 = new Title("Hell Dragon King", "October 31th, 1999", author1);
+		titleRepository.save(title1);
+		Item myItem = new Item(Status.Available, library1, title1);
 		itemRepository.save(myItem);
+		
+		//Test if title is stored by checking its name 
 		myadress=null; 
-		c1=null;
-		l1=null;
-		String id = a1.getAuthorID();
-		a1=null;
-		t1=null;
+		calendar1=null;
+		library1=null;
+		String id = author1.getAuthorID();
+		author1=null;
+		title1=null;
+		
+		//Testing if data is stored successfully with respect to items
 		
 		Title t = titleRepository.findByItem(myItem);
 		
@@ -163,55 +183,61 @@ public class TestTitleRepositoryPersistence {
 	
 	@Test 
 	public void testPersistAndLoadTitleByItemBarCodes() {
-
-		Address ad1 = new Address("3","YellowBirckRoad","Montreal","Oz","Quebec","Canada");
-		addressRepository.save(ad1);
 		
-		Calendar c1 = new Calendar();
-		calendarRepository.save(c1);
+		//create and save data
+		Address address1 = new Address("3","YellowBirckRoad","Montreal","Oz","Quebec","Canada");
+		addressRepository.save(address1);
 		
-		LibrarySystem l1 = new LibrarySystem(ad1, c1);
-		librarySystemRepository.save(l1);
+		Calendar calendar1 = new Calendar();
+		calendarRepository.save(calendar1);
 		
-		Author a1 = new Author("J.K.", "Rowling");
-		authorRepository.save(a1);
+		LibrarySystem library1 = new LibrarySystem(address1, calendar1);
+		librarySystemRepository.save(library1);
 		
-		Author a2 = new Author("Ehsan", "Ahmed");
-		authorRepository.save(a2);
+		Author author1 = new Author("J.K.", "Rowling");
+		authorRepository.save(author1);
 		
-		Title t1 = new Title("Harry Potter", "October 22nd, 1999", a1);
-		titleRepository.save(t1);
+		Author author2 = new Author("Ehsan", "Ahmed");
+		authorRepository.save(author2);
 		
-		Title t2 = new Title("Boy Wonder", "October 22nd, 2000", a2);
-		titleRepository.save(t2);
+		Title title1 = new Title("Harry Potter", "October 22nd, 1999", author1);
+		titleRepository.save(title1);
 		
-		Item i1 = new Item(Status.Available, l1, t1);
-		itemRepository.save(i1);
+		Title title2 = new Title("Boy Wonder", "October 22nd, 2000", author2);
+		titleRepository.save(title2);
 		
-		Item i2 = new Item(Status.Available, l1, t1);
-		itemRepository.save(i2);
+		Item item1 = new Item(Status.Available, library1, title1);
+		itemRepository.save(item1);
 		
-		Item i3 = new Item(Status.Available, l1, t2);
-		itemRepository.save(i3);
+		Item item2 = new Item(Status.Available, library1, title1);
+		itemRepository.save(item2);
 		
-		ad1=null; 
-		c1=null;
-		l1=null;
-		a1=null;
-		a2=null;
-		t1=null;
-		t2=null;
-		String id1 = i1.getItemBarcode();
-		String id2 = i2.getItemBarcode();
-		String id3 = i3.getItemBarcode();
+		Item item3 = new Item(Status.Available, library1, title2);
+		itemRepository.save(item3);
+		
+		//Test if title is stored by checking its name 
+		address1=null; 
+		calendar1=null;
+		library1=null;
+		author1=null;
+		author2=null;
+		title1=null;
+		title2=null;
+		
+		//getting item bar codes
+		String id1 = item1.getItemBarcode();
+		String id2 = item2.getItemBarcode();
+		String id3 = item3.getItemBarcode();
 		
 		List<Item> items = new ArrayList<Item>();
-		items.add(i1);
-		i1=null;
-		items.add(i2);
-		i2=null;
-		items.add(i3);
-		i3=null;
+		items.add(item1);
+		item1=null;
+		items.add(item2);
+		item2=null;
+		items.add(item3);
+		item3=null;
+		
+		//testing whether titles are stored in database properly through a list of items and their item bar codes
 		List<Title> titles = titleRepository.findByItemIn(items);
 		
 		
@@ -223,31 +249,41 @@ public class TestTitleRepositoryPersistence {
 	
 	@Test 
 	public void testPersistAndLoadTitleName() {
-		Author a1 = new Author("J.K.", "Rowling");
-		Title t1 = new Title("Harry Potter and The Philosopher's Stone", "October 31th, 2021", a1);
-		Author a2 = new Author("Dan", "Hosi");
-		Author a3 = new Author("Harsh", "Patel");
-		authorRepository.save(a1);
-		authorRepository.save(a2);
-		authorRepository.save(a3);
-		titleRepository.save(t1);
+		//create data
+		Author author1 = new Author("J.K.", "Rowling");
+		Title title1 = new Title("Harry Potter and The Philosopher's Stone", "October 31th, 2021", author1);
+		Author author2 = new Author("Dan", "Hosi");
+		Author author3 = new Author("Harsh", "Patel");
 		
+		//save data
+		authorRepository.save(author1);
+		authorRepository.save(author2);
+		authorRepository.save(author3);
+		titleRepository.save(title1);
+		
+		
+		//preparing test
 		String name = "Harry Potter and The Philosopher's Stone";
-		t1 = null;
+		title1 = null;
+		//Test if title is stored by checking its name 
 		List<Title> listTitlesByTitleName = titleRepository.findByName(name);
 		assertEquals(name,listTitlesByTitleName.get(0).getName());
 	}
 	
 	@Test 
 	public void testPersistAndLoadTitlePubDate() {
+		//create data
+		Author author1 = new Author("Ehsan", "Ahmed");
+		Title title1 = new Title("Guide to How to Screw Up in Life","October 22nd, 2021", author1);
+		//save data
+		authorRepository.save(author1);		
+		titleRepository.save(title1);
 		
-		Author a1 = new Author("Ehsan", "Ahmed");
-		Title t1 = new Title("Guide to How to Screw Up in Life","October 22nd, 2021", a1);
-		authorRepository.save(a1);		
-		titleRepository.save(t1);
+		//preparing test
 		String pubDate = "October 22nd, 2021";
-
-		t1 = null;
+		
+		//Test if title is stored by checking its published date
+		title1 = null;
 		List<Title> listTitlesByPubDate = titleRepository.findByPubDate(pubDate);
 		assertEquals(pubDate,listTitlesByPubDate.get(0).getPubDate());
 		 
@@ -255,19 +291,22 @@ public class TestTitleRepositoryPersistence {
 	
 	@Test 
 	public void testPersistAndLoadTitleNameAndPubDate() {
-        Author a1 = new Author("Ehsan", "Ahmed");
-        authorRepository.save(a1);
-        Title t1 = new Title("Guide to How to Screw Up in Life","October 22nd, 2021", a1);
-        titleRepository.save(t1);
-        Title t2 = new Title("Guide to How to Screw Up in Life","October 31th, 2021", a1);
-        titleRepository.save(t2);
-
+		//create and save data
+        Author author1 = new Author("Ehsan", "Ahmed");
+        authorRepository.save(author1);
+        Title title1 = new Title("Guide to How to Screw Up in Life","October 22nd, 2021", author1);
+        titleRepository.save(title1);
+        Title title2 = new Title("Guide to How to Screw Up in Life","October 31th, 2021", author1);
+        titleRepository.save(title2);
+        
+      //preparing test
         String name = "Guide to How to Screw Up in Life";
         String pubDate = "October 22nd, 2021"; 
 
-        t1 = null; 
-        t2 = null;
+        title1 = null; 
+        title2 = null;
         
+        //Test if data is stored correctly by checking its name and publish date
         Title titleNamePub = titleRepository.findByNameAndPubDate(name, pubDate);
 
         assertEquals(name, titleNamePub.getName());
