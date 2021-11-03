a6a324be492b68c946a4768bfcfbb1fec417d86f
Majdoub90 <77756961+Majdoub90@users.noreply.github.com>
2021-11-02 23:24:33 -0400

    Services for items and it's subclasses

---
+++ LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/service/ItemService.java
@@ -0,0 +1,136 @@
+package ca.mcgill.ecse321.librarysystem.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import ca.mcgill.ecse321.librarysystem.dao.ItemRepository;
+import ca.mcgill.ecse321.librarysystem.model.Booking;
+import ca.mcgill.ecse321.librarysystem.model.Item;
+import ca.mcgill.ecse321.librarysystem.model.Item.Status;
+import ca.mcgill.ecse321.librarysystem.model.Title;
+
+@Service
+public class ItemService {
+	@Autowired
+	private ItemRepository itemRepository;
+
+	@Transactional
+	public Item createItem(Status aStatus, long aItemBarcode, Title aTitle) {
+		if (aStatus == null || aTitle == null) throw new IllegalArgumentException("Please enter a valid status or title");
+		Item item = new Item(aStatus, aItemBarcode, aTitle);
+		itemRepository.save(item);
+		return item;
+	}
+
+	@Transactional
+	public Item getItemById(Long itemBarcode) {
+		Item item = itemRepository.findItemByItemBarcode(itemBarcode);
+		if (item == null) throw new IllegalArgumentException("No Item found");
+		
+		return item;
+	}
+
+	@Transactional
+	public List<Item> getItemByStat(Status status) {
+		List<Item> item = itemRepository.findItemByStatus(status);
+		for (Item iteme : item) {
+			if (iteme == null) throw new IllegalArgumentException("No Item found");
+		}	
+		return item;
+	}
+
+	@Transactional
+	public List<Item> getItemByTitle(Title title) {
+		List<Item> item = itemRepository.findItemByTitle(title);
+		for (Item iteme : item) {
+			if (iteme == null) throw new IllegalArgumentException("No Item found");
+		}	
+		return item;
+	}
+
+	@Transactional
+	public boolean getexistanceByItemBarcode(Long itemBarcode) {
+		boolean booleanOfItem = itemRepository.existsByItemBarcode(itemBarcode);
+		return booleanOfItem;
+	}
+
+	@Transactional
+	public Item getItemByItemBooking(Booking booking) {
+		Item item = itemRepository.findItemByBooking(booking);
+		if (item == null) throw new IllegalArgumentException("No Item found");
+		return item;
+	}
+	
+	@Transactional
+	public void deleatItemById(Long itemBarcode) {
+		Item item = itemRepository.findItemByItemBarcode(itemBarcode);
+		if (item == null) throw new IllegalArgumentException("No Item found");
+		itemRepository.delete(item);
+		item.delete();
+	}
+
+	@Transactional
+	public void deleatItemByStat(Status status) {
+		List<Item> item = itemRepository.findItemByStatus(status);
+		for (Item iteme : item) {
+			if (iteme == null) throw new IllegalArgumentException("No Item found");
+		}	
+		itemRepository.deleteAll(item);
+		
+		for (Item iteme : item) {
+			iteme.delete();;
+		}
+	}
+
+	@Transactional
+	public void deleatItemByTitle(Title title) {
+		List<Item> item = itemRepository.findItemByTitle(title);
+		for (Item iteme : item) {
+			if (iteme == null) throw new IllegalArgumentException("No Item found");
+		}	
+		itemRepository.deleteAll(item);
+		for (Item iteme : item) {
+			iteme.delete();;
+		}	
+		
+	}
+
+
+	@Transactional
+	public void deleatItemByItemBooking(Booking booking) {
+		Item item = itemRepository.findItemByBooking(booking);
+		if (item == null) throw new IllegalArgumentException("No Item found");
+		itemRepository.delete(item);
+		item.delete();
+	}
+	
+	
+	@Transactional 
+	public void updateItem(Status aStatus, long aItemBarcode, Title aTitle) {
+		Item item = itemRepository.findItemByItemBarcode(aItemBarcode);
+		if (item == null) throw new IllegalArgumentException("No Item found");
+		item.setStatus(aStatus);
+		item.setTitle(aTitle);
+		itemRepository.save(item);
+	}
+	
+	@Transactional 
+	public void updateItem(long aItemBarcode, Title aTitle) {
+		Item item = itemRepository.findItemByItemBarcode(aItemBarcode);
+		if (item == null) throw new IllegalArgumentException("No Item found");
+		item.setTitle(aTitle);
+		itemRepository.save(item);
+	}
+	
+	@Transactional 
+	public void updateItem(Status aStatus,long aItemBarcode) {
+		Item item = itemRepository.findItemByItemBarcode(aItemBarcode);
+		if (item == null) throw new IllegalArgumentException("No Item found");
+		item.setStatus(aStatus);
+		itemRepository.save(item);
+	}
+
+}
---
+++ LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/service/NewspaperService.java
@@ -0,0 +1,141 @@
+package ca.mcgill.ecse321.librarysystem.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import ca.mcgill.ecse321.librarysystem.dao.NewspaperRepository;
+import ca.mcgill.ecse321.librarysystem.model.Booking;
+import ca.mcgill.ecse321.librarysystem.model.Item;
+import ca.mcgill.ecse321.librarysystem.model.Item.Status;
+import ca.mcgill.ecse321.librarysystem.model.Newspaper;
+
+import ca.mcgill.ecse321.librarysystem.model.Title;
+
+@Service
+public class NewspaperService {
+	@Autowired
+	private NewspaperRepository newspaperRepository;
+
+	@Transactional
+	public Newspaper createNewspaper(Status aStatus, long aNewspaperBarcode, Title aTitle) {
+		if (aStatus == null || aTitle == null) throw new IllegalArgumentException("Please enter a valid status or title");
+		Newspaper item = new Newspaper(aStatus, aNewspaperBarcode, aTitle);
+		newspaperRepository.save(item);
+		return item;
+	}
+
+	@Transactional
+	public Newspaper getNewspaperById(Long itemBarcode) {
+		Newspaper item = (Newspaper) newspaperRepository.findItemByItemBarcode(itemBarcode);
+		if (item == null) throw new IllegalArgumentException("No Newspaper found");
+		
+		return item;
+	}
+
+	@Transactional
+	public List<Newspaper> getNewspaperByStat(Status status) {
+		@SuppressWarnings("unchecked")
+		List<Newspaper> item = (List<Newspaper>)(List<?>) newspaperRepository.findItemByStatus(status);
+		for (Newspaper iteme : item) {
+			if (iteme == null) throw new IllegalArgumentException("No Newspaper found");
+		}	
+		return item;
+	}
+
+	@Transactional
+	public List<Newspaper> getNewspaperByTitle(Title title) {
+		@SuppressWarnings("unchecked")
+		List<Newspaper> item = (List<Newspaper>)(List<?>) newspaperRepository.findItemByTitle(title);
+		for (Newspaper iteme : item) {
+			if (iteme == null) throw new IllegalArgumentException("No Newspaper found");
+		}	
+		return item;
+	}
+
+	@Transactional
+	public boolean getexistanceByNewspaperBarcode(Long itemBarcode) {
+		boolean booleanOfNewspaper = newspaperRepository.existsByItemBarcode(itemBarcode);
+		return booleanOfNewspaper;
+	}
+
+	@Transactional
+	public Newspaper getNewspaperByNewspaperBooking(Booking booking) {
+		Newspaper item = (Newspaper) newspaperRepository.findItemByBooking(booking);
+		if (item == null) throw new IllegalArgumentException("No Newspaper found");
+		return item;
+	}
+	
+	@Transactional
+	public void deleatNewspaperById(Long itemBarcode) {
+		Newspaper item = (Newspaper) newspaperRepository.findItemByItemBarcode(itemBarcode);
+		if (item == null) throw new IllegalArgumentException("No Newspaper found");
+		newspaperRepository.delete(item);
+		item.delete();
+	}
+
+	@Transactional
+	public void deleatNewspaperByStat(Status status) {
+		@SuppressWarnings("unchecked")
+		List<Newspaper> item = (List<Newspaper>)(List<?>) newspaperRepository.findItemByStatus(status);
+		for (Newspaper iteme : item) {
+			if (iteme == null) throw new IllegalArgumentException("No Newspaper found");
+		}	
+		newspaperRepository.deleteAll(item);
+		for (Item iteme : item) {
+			iteme.delete();;
+		}	
+	}
+
+	@Transactional
+	public void deleatNewspaperByTitle(Title title) {
+		@SuppressWarnings("unchecked")
+		List<Newspaper> item = (List<Newspaper>)(List<?>) newspaperRepository.findItemByTitle(title);
+		for (Newspaper iteme : item) {
+			if (iteme == null) throw new IllegalArgumentException("No Newspaper found");
+		}	
+		newspaperRepository.deleteAll(item);
+		
+		for (Item iteme : item) {
+			iteme.delete();;
+		}	
+	}
+
+
+	@Transactional
+	public void deleatNewspaperByNewspaperBooking(Booking booking) {
+		Newspaper item = (Newspaper) newspaperRepository.findItemByBooking(booking);
+		if (item == null) throw new IllegalArgumentException("No Newspaper found");
+		newspaperRepository.delete(item);
+		item.delete();
+	}
+	
+	
+	@Transactional 
+	public void updateNewspaper(Status aStatus, long aNewspaperBarcode, Title aTitle) {
+		Newspaper item = (Newspaper) newspaperRepository.findItemByItemBarcode(aNewspaperBarcode);
+		if (item == null) throw new IllegalArgumentException("No Newspaper found");
+		item.setStatus(aStatus);
+		item.setTitle(aTitle);
+		newspaperRepository.save(item);
+	}
+	
+	@Transactional 
+	public void updateNewspaper(long aNewspaperBarcode, Title aTitle) {
+		Newspaper item = (Newspaper) newspaperRepository.findItemByItemBarcode(aNewspaperBarcode);
+		if (item == null) throw new IllegalArgumentException("No Newspaper found");
+		item.setTitle(aTitle);
+		newspaperRepository.save(item);
+	}
+	
+	@Transactional 
+	public void updateNewspaper(Status aStatus,long aNewspaperBarcode) {
+		Newspaper item = (Newspaper) newspaperRepository.findItemByItemBarcode(aNewspaperBarcode);
+		if (item == null) throw new IllegalArgumentException("No Newspaper found");
+		item.setStatus(aStatus);
+		newspaperRepository.save(item);
+	}
+
+}
