ed5b18c4d8772d1e93d70898fcd327802c9bc719
AlexandruB30 <73802110+AlexandruB30@users.noreply.github.com>
2021-11-08 01:04:42 -0500

    Implemented JavaScript testing for Axios

---
+++ LibrarySystem-Backend/src/test/javascript/CustomerTest.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+// const customerTest = async () => {
+//     try {
+//         let postTest = await axios.post("http://localhost:8080/customer/createLocal", {
+//             firstname: "John",
+//             lastname: "Doe",
+//             civic: "1",
+//             street: "University",
+//             city: "Montreal",
+//             postalCode: "H1X 1S4",
+//             province: "QC",
+//             country: "CA"
+//         });
+//     }
+//     catch (error) {
+//         console.log("Customer test unsuccessful:");
+//         console.log(`${error}`);
+//     }
+// };
+//
+// export default customerTest;
