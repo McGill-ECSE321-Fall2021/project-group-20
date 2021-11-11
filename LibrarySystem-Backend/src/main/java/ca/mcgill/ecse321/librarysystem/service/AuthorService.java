fdc1ed6fd9c9a0075e2aa48bbdbb4ccdef1d17d2
royalblaster <ehsanahmed99@gmail.com>
2021-11-11 17:51:31 -0500

    Add comments to AuthorService

--- LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/service/AuthorService.java
+++ LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/service/AuthorService.java
@@ -17,14 +17,21 @@ public class AuthorService {
 
     @Autowired
     AuthorRepository authorRepository;
-
+    
+    
+    /*
+    Creates a new Author using the default constructor
+     */
     @Transactional
     public Author createAuthor() {
         Author author = new Author();
         authorRepository.save(author);
         return author;
     } 
-
+    
+    /*
+    Creates a new Author using first and last name
+     */
     @Transactional
     public Author createAuthor(String firstName, String lastName) {
     	if (firstName == null || lastName == null || firstName.length() == 0 || lastName.length() == 0) throw new IllegalArgumentException("Please enter a valid name");
@@ -33,7 +40,9 @@ public class AuthorService {
         return author; 
     }
 
-//	I don't think we need this method because when we save author to the database, the id is auto-generated
+    /*
+    Creates a new Author using id and first and last names
+     */
     @Transactional
     public Author createAuthor(String authorID, String firstName, String lastName) {
     	if (authorID == null || authorID.length() == 0) throw new IllegalArgumentException("Please enter a valid author id"); 
@@ -42,7 +51,10 @@ public class AuthorService {
         authorRepository.save(author);
         return author;
     }
-
+    
+    /*
+    Get Author by author id
+     */
     @Transactional
     public Author getAuthorByAuthorID(String authorID) {
     	if (authorID == null || authorID.length() == 0) throw new IllegalArgumentException("Please enter a valid author id"); 
@@ -50,7 +62,10 @@ public class AuthorService {
         if (author == null) throw new NullPointerException("Author not found");
         return author;
     }
-
+    
+    /*
+    Get all the Authors that have the same fist name 
+     */
     @Transactional
     public List<Author> getAuthorsByFirstName(String firstName) {
     	if (firstName == null || firstName.length() == 0) throw new IllegalArgumentException("Please enter a valid first name");
@@ -58,7 +73,10 @@ public class AuthorService {
         if (authors.size() == 0) throw new IllegalArgumentException("Authors not found");
         return authors;
     }
-
+    
+    /*
+    Get all the Authors that have the same last name 
+     */
     @Transactional
     public List<Author> getAuthorsByLastName(String lastName) {
     	if (lastName == null || lastName.length() == 0) throw new IllegalArgumentException("Please enter a valid last name");
@@ -66,7 +84,10 @@ public class AuthorService {
         if (authors.size() == 0) throw new IllegalArgumentException("Authors not found");
         return authors;
     }
-
+    
+    /*
+    Get all the Authors that have the same fist and last name 
+     */
     @Transactional
     public List<Author> getAuthorsByFirstNameAndLastName(String firstName, String lastName) {
     	if (firstName == null || lastName == null || firstName.length() == 0 || lastName.length() == 0) throw new IllegalArgumentException("Please enter a valid name");
@@ -74,8 +95,10 @@ public class AuthorService {
         if (authors.size() == 0) throw new IllegalArgumentException("Authors not found");
         return authors;
     }
-
-
+    
+    /*
+    Get all the Authors that have the given titles
+     */
     @Transactional
     public List<Author> getAuthorsByTitles(List<Title> titles) {
     	if (titles == null || titles.size() == 0) throw new IllegalArgumentException("Please enter a valid list of titles");
@@ -84,6 +107,9 @@ public class AuthorService {
         return authors;
     }
     
+    /*
+    Updates an author's first name given his id and the new first name
+     */
     @Transactional
     public boolean updateFirstName(String authorID, String firstName) {
     	Author author = getAuthorByAuthorID(authorID);
@@ -95,6 +121,9 @@ public class AuthorService {
         return false;
     }
     
+    /*
+    Updates an author's last name given his id and the new last name
+     */
     @Transactional
     public boolean updateLastName(String authorID, String lastName) {
     	Author author = getAuthorByAuthorID(authorID);
@@ -106,6 +135,9 @@ public class AuthorService {
         return false;
     }
     
+    /*
+    Updates an author's first and last name given his author id, the new first name and the new last name
+     */
     @Transactional
     public boolean updateFullName(String authorID, String firstName, String lastName) {
     	Author author = getAuthorByAuthorID(authorID);
@@ -117,7 +149,9 @@ public class AuthorService {
         return false;
     }
     
-
+    /*
+    Deletes the author given its id
+     */
     @Transactional
     public boolean deleteAuthorByAuthorID(String authorID) {
     	if (authorID == null || authorID.length() == 0) throw new IllegalArgumentException("Please enter a valid author id"); 
@@ -128,7 +162,10 @@ public class AuthorService {
         Author test = authorRepository.findByAuthorID(authorID);
         return (test == null);
     }
-
+    
+    /*
+    Deletes the authors having the given first name 
+     */
     @Transactional
     public boolean deleteAuthorsByFirstName(String firstName) {
     	if (firstName == null || firstName.length() == 0) throw new IllegalArgumentException("Please enter a valid first name");
@@ -141,7 +178,10 @@ public class AuthorService {
         List<Author> test = authorRepository.findByFirstName(firstName);
         return (test.size() == 0);
     }
-
+    
+    /*
+    Deletes the authors having the given last name 
+     */
     @Transactional
     public boolean deleteAuthorsByLastName(String lastName) {
     	if (lastName == null || lastName.length() == 0) throw new IllegalArgumentException("Please enter a valid last name");
@@ -154,7 +194,10 @@ public class AuthorService {
         List<Author> test = authorRepository.findByLastName(lastName);
         return (test.size() == 0);
     }
-
+    
+    /*
+    Deletes the authors with the first and last name given
+     */
     @Transactional
     public boolean deleteAuthorsByFirstNameAndLastName(String firstName, String lastName) {
     	if (firstName == null || lastName == null || firstName.length() == 0 || lastName.length() == 0) throw new IllegalArgumentException("Please enter a valid name");
@@ -167,8 +210,10 @@ public class AuthorService {
         List<Author> test = authorRepository.findByFirstNameAndLastName(firstName, lastName);
         return (test.size() == 0);
     }
-
-
+    
+    /*
+    Deletes all the authors that have titles in given list of titles
+     */
     @Transactional
     public boolean deleteAuthorsByTitles(List<Title> titles) {
     	if (titles == null || titles.size() == 0) throw new IllegalArgumentException("Please enter a valid list of titles");
@@ -181,32 +226,46 @@ public class AuthorService {
         List<Author> test = authorRepository.findByTitlesIn(titles);
         return (test.size() == 0);
     }
-
-
+    
+    /*
+    Determines if an author exists given a first name
+     */
     @Transactional
     public boolean isAuthorsExistsByFirstName(String firstName) {
     	if (firstName == null || firstName.length() == 0) throw new IllegalArgumentException("Please enter a valid first name");
         return (authorRepository.existsByFirstName(firstName));
     }
-
+    
+    /*
+    Determines if an author exists given a last name
+     */
     @Transactional
     public boolean isAuthorsExistsByLastName(String lastName) {
     	if (lastName == null || lastName.length() == 0) throw new IllegalArgumentException("Please enter a valid last name");
         return (authorRepository.existsByLastName(lastName));
     }
-
+    
+    /*
+    Determines if an author exists given a first and last name
+     */
     @Transactional
     public boolean isAuthorsExistsByFirstNameAndLastName(String firstName, String lastName) {
     	if (firstName == null || lastName == null || firstName.length() == 0 || lastName.length() == 0) throw new IllegalArgumentException("Please enter a valid name");
         return (authorRepository.existsByFirstNameAndLastName(firstName, lastName));
     }
-
+    
+    /*
+    Determines if an author exists given an author id
+     */
     @Transactional
     public boolean isAuthorsExistsByAuthorID(String authorID) {
     	if (authorID == null || authorID.length() == 0) throw new IllegalArgumentException("Please enter a valid author id"); 
         return (authorRepository.existsByAuthorID(authorID));
     }
-
+    
+    /*
+    Get all authors from the database
+     */
     @Transactional 
     public List<Author> getAuthors() {
         List<Author> authors = new ArrayList<>();
