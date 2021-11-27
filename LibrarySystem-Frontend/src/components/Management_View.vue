3ba40295d066a39c56930a084cf17509c35a5fce
royalblaster <ehsanahmed99@gmail.com>
2021-11-27 18:31:29 -0500

    Management view page done + advance js

--- LibrarySystem-Frontend/src/components/Management_View.vue
+++ LibrarySystem-Frontend/src/components/Management_View.vue
@@ -1,166 +1,122 @@
 <template>
 <div id="Management" >
-  <br>
-  <br>
-  <div class="Management">
-    <h2> <b>Library System Inventory</b></h2>
+  <div class="header_img">
+    <img src="../assets/LS.png" style="width:20vh; height:auto; padding-bottom: 2vh">
   </div>
-  <br>
-  <br>
-  <br>
-  <div>
 
-    <div id="main_picture_container">
-      <img src="../assets/books_management.jpg" width="304" height="236">
-      <img src="../assets/film_management.jpg" width="304" height="236">
-      <img src="../assets/music_management.jpg" width="304" height="236">
-    </div>
+  <div class="mainFrame">
 
+    <div><h2><b>Add to Library Inventory</b></h2></div>
     <br>
-    <br>
+    <b-tabs content-class="mt-3" align="center">
 
-    <div class="seb">
-      <form>
-        <label><b>Author:</b></label>
-        <input type="text" v-model="firstname" id="firstname" class="form-control" placeholder="First Name" required>
+      <div class="global">
+        <label><b>Author Info: </b></label>
+        <input value="response.firstname" type="text" v-model="firstname" id="firstname" placeholder="First Name" class="form-control" required>
+        <input value="response.lastname" type="text" v-model="lastname" id="lastname" placeholder="Last Name" class="form-control" required>
         <br>
-        <input type="text" v-model="lastname" id="lastname" class="form-control" placeholder="Last Name" required>
+        <label><b>Title Info: </b></label>
+        <input v-model="titlename" id="titlename" placeholder="Title Name" class="form-control" required>
+        <input v-model="publishdate" id="publishdate" placeholder="Publish Date (mm/dd/yyyy)" class="form-control" required>
         <br>
+      </div>
+
+      <b-tab title="Archive" active>
+        <div class="inputFrame">
+          <label><b>Item Status</b></label>
+          <select name="item_status" id="archive_item_status">
+            <option selected value="Available">Available</option>
+            <option value="Damaged">Damaged</option>
+          </select>
+        </div>
         <br>
-<!--        <br>-->
-<!--        <br>-->
-<!--        <label><b>Item:</b></label>-->
-<!--&lt;!&ndash;        <input type="text" v-model="itemstatus" id="itemstatus" class="form-control" placeholder="Item Status" required>&ndash;&gt;-->
-<!--        <select class="form-select" aria-label="Default select example">-->
-<!--          <option selected>Open this select menu</option>-->
-<!--          <option value="Available">Available</option>-->
-<!--          <option value="Reserved">Reserved</option>-->
-<!--          <option value="Borrowed">Borrowed</option>-->
-<!--          <option value="Damaged">Damaged</option>-->
-<!--        </select>-->
-<!--        <br>-->
-<!--        <br>-->
-<!--        <button class="btn btn-danger" id="create-button" @click="create">Create</button>-->
+        <div class="buttonArchiveFrame inputLabel">
+          <button class="btn-success-acc" @click="createArchive(firstname, lastname, titlename, publishdate, archive_item_status)">Create Archive
+          </button>
+        </div>
+      </b-tab>
+
+      <b-tab title="Book">
+        <div class="inputFrame">
+          <label><b>ISBN code:</b></label>
+          <input v-model="isbncode" id="isbncode" placeholder="ISBN" class="form-control" required>
+          <br>
+          <label><b>Number of Pages: </b></label>
+          <input v-model="numberpages" id="numberpages" placeholder="Number of pages" class="form-control" required>
+          <br>
+          <label><b>Item Status</b></label>
+          <select name="item_status" id="book_item_status">
+            <option selected value="Available">Available</option>
+            <option value="Reserved">Reserved</option>
+            <option value="Borrowed">Borrowed</option>
+            <option value="Damaged">Damaged</option>
+          </select>
+        </div>
         <br>
-      </form>
-      <form>
+        <div class="buttonBookFrame inputPersLabel">
+          <button class="btn-success-acc" @click="createBook(firstname, lastname, titlename, publishdate, book_item_status, isbncode, numberpages)">Create Book
+          </button>
+        </div>
+
+      </b-tab>
+      <b-tab title="Movie" active>
+        <div class="inputFrame">
+          <label><b>Movie Length: </b></label>
+          <input v-model="lengthmovie" id="lengthmovie" placeholder="Length in minutes" class="form-control" required>
+          <br>
+          <label><b>Item Status</b></label>
+          <select name="movie_item_status" id="movie_item_status">
+            <option selected value="Available">Available</option>
+            <option value="Reserved">Reserved</option>
+            <option value="Borrowed">Borrowed</option>
+            <option value="Damaged">Damaged</option>
+          </select>
+        </div>
         <br>
+        <div class="buttonMovieFrame inputLabel">
+          <button class="btn-success-acc" @click="createMovie(firstname, lastname, titlename, publishdate, movie_item_status, lengthmovie)">Create Movie
+          </button>
+        </div>
+
+      </b-tab>
+
+      <b-tab title="Music Album" active>
+        <div class="inputFrame">
+          <label><b>Music Album Length: </b></label>
+          <input v-model="lengthmusic" id="lengthmusic" placeholder="Length in minutes" class="form-control" required>
+          <br>
+          <label><b>Item Status</b></label>
+          <select name="music_item_status" id="music_item_status">
+            <option selected value="Available">Available</option>
+            <option value="Reserved">Reserved</option>
+            <option value="Borrowed">Borrowed</option>
+            <option value="Damaged">Damaged</option>
+          </select>
+        </div>
         <br>
-                <label><b>Item:</b></label>
-                <select class="form-select" aria-label="Default select example">
-                  <option selected>Open this select menu</option>
-                  <option value="Available">Available</option>
-                  <option value="Reserved">Reserved</option>
-                  <option value="Borrowed">Borrowed</option>
-                  <option value="Damaged">Damaged</option>
-                </select>
+        <div class="buttonMusicAlbumFrame inputLabel">
+          <button class="btn-success-acc" @click="createMusicAlbum(firstname, lastname, titlename, publishdate, music_item_status, lengthmusic)">Create Music Album
+          </button>
+        </div>
 
-      </form>
-      <form>
-        <label><b>Title:</b></label>
-        <input type="text" v-model="titlename" id="titlename" class="form-control" placeholder="Title Name" required>
+      </b-tab>
+
+      <b-tab title="Newspaper" active>
+        <div class="inputFrame">
+          <label><b>Item Status</b></label>
+          <select name="newspaper_item_status" id="newspaper_item_status">
+            <option selected value="Available">Available</option>
+            <option value="Damaged">Damaged</option>
+          </select>
+        </div>
         <br>
-        <input type="text" v-model="publishdate" id="publishdate" class="form-control" placeholder="Publish Date" required>
-      </form>
-    </div>
-
-    <div>
-      <button class="btn btn-danger" id="create-button" @click="create">Create</button>
-    </div>
-
-
-
-
-<!--    <b-container class="input">-->
-<!--      <b-row>-->
-<!--        <b-col>-->
-<!--          <label>Firstname:</label>-->
-<!--          <input type="text" v-model="firstname" id="firstname" class="form-control" required>-->
-<!--        </b-col>-->
-<!--        <b-col>-->
-<!--          <label>Lastname:</label>-->
-<!--          <input type="text" v-model="lastname" id="lastname" class="form-control" required>-->
-<!--        </b-col>-->
-<!--      </b-row>-->
-<!--      <br>-->
-<!--      <b-row>-->
-<!--        <b-col>-->
-<!--          <label for="civic">Civic Number:</label>-->
-<!--          <input type="text" v-model="civic" id="civic" class="form-control" required>-->
-<!--        </b-col>-->
-<!--        <b-col>-->
-<!--          <label for="street">Street:</label>-->
-<!--          <input type="text" v-model="street" id="street" class="form-control" required>-->
-<!--        </b-col>-->
-<!--        <b-col>-->
-<!--          <label for="city">City:</label>-->
-<!--          <input type="text" v-model="city" id="city" class="form-control" required>-->
-<!--        </b-col>-->
-<!--      </b-row>-->
-<!--      <br>-->
-<!--      <b-row>-->
-<!--        <b-col>-->
-<!--          <label for="username">Username:</label>-->
-<!--          <input type="text" v-model="username" id="username" class="form-control" required>-->
-<!--        </b-col>-->
-<!--        <b-col>-->
-<!--          <label for="password">Password:</label>-->
-<!--          <input type="password" v-model="password" id="password" class="form-control" required>-->
-<!--        </b-col>-->
-<!--      </b-row>-->
-<!--    </b-container>-->
-
-
-
-<!--    <b-navbar toggleable="lg" type="dark" variant="info" >-->
-
-<!--        <b-navbar-toggle target="nav-collapse">-->
-<!--        </b-navbar-toggle>-->
-
-<!--        <b-collapse id="nav-collapse" is-nav>-->
-<!--          <b-navbar-nav class="mx-auto">-->
-<!--            <b-nav-item-dropdown text="Lang" left>-->
-<!--              <b-dropdown-item href="#">Author</b-dropdown-item>-->
-<!--              <b-dropdown-item href="#">Item</b-dropdown-item>-->
-<!--              <b-dropdown-item href="#">RU</b-dropdown-item>-->
-<!--              <b-dropdown-item href="#">FA</b-dropdown-item>-->
-<!--            </b-nav-item-dropdown>-->
-<!--            <b-nav-form >-->
-<!--              <b-form-input size="sm" class="mr-sm-2 w-100%" placeholder="Search"></b-form-input>-->
-<!--              <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>-->
-<!--            </b-nav-form>-->
-<!--          </b-navbar-nav>-->
-
-<!--          &lt;!&ndash; Right aligned nav items &ndash;&gt;-->
-
-<!--        </b-collapse>-->
-<!--      </b-navbar>-->
-
-<!--  <b-container >-->
-<!--    <b-row>-->
-<!--&lt;!&ndash;      <b-col align-h="center" class="input_columns">&ndash;&gt;-->
-<!--&lt;!&ndash;        <select v-model="selected" class="select">&ndash;&gt;-->
-<!--&lt;!&ndash;          <option disabled value=""></option>&ndash;&gt;-->
-<!--&lt;!&ndash;          <option>Archive</option>&ndash;&gt;-->
-<!--&lt;!&ndash;          <option>Author</option>&ndash;&gt;-->
-<!--&lt;!&ndash;          <option>Book</option>&ndash;&gt;-->
-<!--&lt;!&ndash;          <option>Item</option>&ndash;&gt;-->
-<!--&lt;!&ndash;          <option>Movie</option>&ndash;&gt;-->
-<!--&lt;!&ndash;          <option>Music Album</option>&ndash;&gt;-->
-<!--&lt;!&ndash;          <option>Newspaper</option>&ndash;&gt;-->
-<!--&lt;!&ndash;          <option>Title</option>&ndash;&gt;-->
-<!--&lt;!&ndash;        </select>&ndash;&gt;-->
-<!--&lt;!&ndash;        <span>{{ selected }}</span>&ndash;&gt;-->
-<!--&lt;!&ndash;      </b-col>&ndash;&gt;-->
-<!--      <b-col align-h="center" class="input_columns" md="auto">-->
-<!--        <input type="text" v-model="input" id="search" class="form-control" required>-->
-<!--      </b-col>-->
-<!--      <b-col align-h="center" class="input_columns" md="auto">-->
-<!--        <button></button>-->
-<!--        </b-col>-->
-<!--    </b-row>-->
-<!--    <br>-->
-<!--  </b-container>-->
+        <div class="buttonNewspaperFrame inputLabel">
+          <button class="btn-success-acc" @click="createNewsPaper(firstname, lastname, titlename, publishdate, newspaper_item_status)">Create Newspaper
+          </button>
+        </div>
+      </b-tab>
+    </b-tabs>
+    <span v-if="error" style="color: red">Error: {{error}}</span>
   </div>
 
 </div>
@@ -174,39 +130,34 @@ export default {
 </script>
 
 <style scoped>
-.select {
+
+.global {
+  width: 24.75%;
+  min-width: 400px;
+  position: relative;
+  left: 50%;
+  transform: translateX(-50%);
   padding-top: 2vh;
 }
 
-.input_columns {
-  padding-right: 0;
-  padding-left: 0;
-  width : 100px;
-  display: inline;
+.inputFrame {
+  width: 24.75%;
+  min-width: 400px;
+  position: relative;
+  left: 50%;
+  transform: translateX(-50%);
+  padding-top: 1vh;
 }
 
-.padding-0 {
-  padding-right: 0;
-  padding-left: 0;
+.btn-success-acc {
+  border-radius: 60px;
+  border: None;
+  width: 150px;
+  height: 55px;
+  color: #FDEDEC;
+  background-color: #a20000 ;
 }
 
-#Management {
-  background-color: rgb(255, 216, 167);
-}
 
-#main_picture_container {
-  display:flex;
-  justify-content: space-evenly;
-}
-
-.seb {
-  display:flex;
-  justify-content: space-evenly;
-  width:100em;
-}
-
-#create-button {
-  border-radius: 25%;
-}
 
 </style>
---
+++ LibrarySystem-Frontend/src/components/management_view_script.js
@@ -0,0 +1,77 @@
+import axios from 'axios'
+import JQuery from 'jquery'
+
+let $ = JQuery
+var config = require('../../config')
+
+var frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
+var backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
+
+var AXIOS = axios.create({
+  baseURL: backendUrl,
+  headers: { 'Access-Control-Allow-Origin': frontendUrl }
+})
+
+export default {
+  name: 'management_view_script',
+  data() {
+    return {
+      error: '',
+      responseAuthor: [],
+      responseTitle: [],
+      responseItem: []
+    }
+  },
+
+  methods: {
+
+    createArchive: function (firstname, lastname, titlename, publishdate, archive_item_status) {
+      if (document.getElementById('firstname').value === '') {
+        this.error = "Please enter a valid first name"
+      }
+
+      else if (document.getElementById('lastname').value === '') {
+        this.error = "Please enter a valid last name"
+      }
+
+      else if (document.getElementById('titlename').value === '') {
+        this.error = "Please enter a valid title name"
+      }
+
+      else if (document.getElementById('publishdate').value === '') {
+        this.error = "Please enter a valid publish date"
+      }
+
+      else {
+
+        AXIOS.post(backendUrl + '/author/create?firstname=' + firstname + '&lastname=' + lastname).then(response => {
+          this.responseAuthor = response.data
+          AXIOS.post(backendUrl + '/title/create?name=' + titlename + '&publishdate=' + publishdate + '&authors=' + this.responseAuthor.authorID).then(response =>
+            {
+              this.responseTitle = response.data
+              AXIOS.post(backendUrl+'/items/create?status=' + archive_item_status + '&titleId=' + this.responseTitle.titleID).then(response =>
+                {
+                  this.responseItem = response.data
+                }
+              ).catch(msg =>
+                {
+                  console.log(msg.response.data)
+                  this.error = msg.response.data;
+                }
+              )
+              //this.error = ''
+              console.log(repsonse)
+            }).catch(msg => {
+              console.log(msg.response.data)
+            if (this.error === '') this.error = msg.response.data;
+            })
+          //this.error = ''
+          console.log(response)
+        }).catch(msg => {
+          console.log(msg.response.data)
+          if (this.error === '') this.error = msg.response.data;
+        })
+      }
+    }
+  }
+}
