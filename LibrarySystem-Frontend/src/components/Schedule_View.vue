641af5ec0e05743a7950bfd7ff28dd591f831766
Danh4160 <63082166+Danh4160@users.noreply.github.com>
2021-11-30 03:43:21 -0500

    Schedule view for employee shifts

--- LibrarySystem-Frontend/src/components/Schedule_View.vue
+++ LibrarySystem-Frontend/src/components/Schedule_View.vue
@@ -7,18 +7,8 @@
 
     <h2 class="main_title">
       <b>View Employee Shifts</b>
-
     </h2>
 
-    <div class="calendar">
-      <b-calendar v-model="value" :date-disabled-fn="dateDisabled" locale="en" class="wrapper"></b-calendar>
-    </div>
-
-    <div class="shiftTable">
-      <b-table striped outline hover :items="itemsTitles">
-
-      </b-table>
-    </div>
 <!--    <hr>-->
 <!--      <h2>Employee Shifts</h2>-->
 <!--      <table>-->
@@ -37,64 +27,189 @@
 <!--      </table>-->
 <!--    <hr>-->
 
+    <b-tabs content-class="mt-3" align="center">
 
+      <b-tab title="Monday" id="Monday" active>
+          <table>
+            <div class="Fields">
+              <tr>
+                <th>Name</th>
+                <th>Employee ID</th>
+                <th>Start Time</th>
+                <th>End Time</th>
+              </tr>
+            </div>
 
+            <tr>
+<!--              <td>{{mondayShifts.firstName}}</td>-->
+<!--&lt;!&ndash;              <td>{{mondayShifts.employee.firstName}}</td>&ndash;&gt;-->
+<!--&lt;!&ndash;              <td>{{mondayShifts.employee.libraryCardID}}</td>&ndash;&gt;-->
+<!--              <td>{{mondayShifts.startTime}}</td>-->
+<!--              <td>{{mondayShifts.endTime}}</td>-->
+            </tr>
+
+          </table>
+
+        <div class="buttonFrame">
+          <button class="btn" @click="getMondayShifts()">View</button>
+        </div>
+
+      </b-tab>
+
+      <b-tab title="Tuesday" id="Tuesday" active>
+
+        <table>
+
+          <div class="Fields">
+            <tr>
+              <th>Name</th>
+              <th>Employee ID</th>
+              <th>Start Time</th>
+              <th>End Time</th>
+            </tr>
+          </div>
+
+<!--          <tr>-->
+<!--            <td>{{tuesdayShifts.employee.firstName}}</td>-->
+<!--            <td>{{tuesdayShifts.employee.libraryCardID}}</td>-->
+<!--            <td>{{tuesdayShifts.startTime}}</td>-->
+<!--            <td>{{tuesdayShifts.endTime}}</td>-->
+<!--          </tr>-->
+
+        </table>
+
+        <div class="buttonFrame">
+          <button class="btn" @click="getTuesdayShifts()">View</button>
+        </div>
+
+      </b-tab>
+
+      <b-tab title="Wednesday" id="Wednesday" active>
+
+        <table>
+
+          <div class="Fields">
+            <tr>
+              <th>Name</th>
+              <th>Employee ID</th>
+              <th>Start Time</th>
+              <th>End Time</th>
+            </tr>
+          </div>
+
+<!--          <tr>-->
+<!--            <td>{{wednesdayShifts.employee.firstName}}</td>-->
+<!--            <td>{{wednesdayShifts.employee.libraryCardID}}</td>-->
+<!--            <td>{{wednesdayShifts.startTime}}</td>-->
+<!--            <td>{{wednesdayShifts.endTime}}</td>-->
+<!--          </tr>-->
+        </table>
+
+        <div class="buttonFrame">
+          <button class="btn" @click="getWednesdayShifts()">View</button>
+        </div>
+
+      </b-tab>
+
+      <b-tab title="Thursday" id="Thursday" active>
+
+        <table>
+
+          <div class="Fields">
+            <tr>
+              <th>Name</th>
+              <th>Employee ID</th>
+              <th>Start Time</th>
+              <th>End Time</th>
+            </tr>
+          </div>
+
+<!--          <tr>-->
+<!--            <td>{{thursdayShifts.employee.firstName}}</td>-->
+<!--            <td>{{thursdayShifts.employee.libraryCardID}}</td>-->
+<!--            <td>{{thursdayShifts.startTime}}</td>-->
+<!--            <td>{{thursdayShifts.endTime}}</td>-->
+<!--          </tr>-->
+        </table>
+
+        <div class="buttonFrame">
+          <button class="btn" @click="getThursdayShifts()">View</button>
+        </div>
+
+      </b-tab>
+
+      <b-tab title="Friday" id="Friday" active>
+
+        <table>
+
+          <div class="Fields">
+            <tr>
+              <th>Name</th>
+              <th>Employee ID</th>
+              <th>Start Time</th>
+              <th>End Time</th>
+            </tr>
+          </div>
+
+<!--          <tr>-->
+<!--            <td>{{fridayShifts.employee.firstName}}</td>-->
+<!--            <td>{{fridayShifts.employee.libraryCardID}}</td>-->
+<!--            <td>{{fridayShifts.startTime}}</td>-->
+<!--            <td>{{fridayShifts.endTime}}</td>-->
+<!--          </tr>-->
+        </table>
+
+        <div class="buttonFrame">
+          <button class="btn" @click="getFridayShifts()">View</button>
+        </div>
+
+      </b-tab>
+    </b-tabs>
+  <span v-if="error" style="color:red"> Error: {{error}}</span>
 
   </div>
 </template>
 
-<script>
+
+<script src="./schedule_view_script.js">
 export default {
-  data() {
-    return {
-      value: '',
-      fields: [
-        {
-          key: 'Name'
-        },
-
-        {
-          key: 'Employee ID'
-        },
-
-        {
-          key: 'Start Time'
-        },
-
-        {
-          key: 'End Time'
-        }
-      ]
-    }
-  },
-  methods: {
-    dateDisabled(ymd, date) {
-      // Disable weekends (Sunday = `0`, Saturday = `6`) and
-      // disable days that fall on the 13th of the month
-      const weekday = date.getDay()
-      const day = date.getDate()
-      // Return `true` if the date should be disabled
-      return weekday === 0 || weekday === 6 || day === 13
-    }
-  }
+  name: "Schedule_view",
+  Monday: 'Monday'
 }
+
 </script>
 
 <style scoped>
 
-.calendar {
-  width: 50%;
-  min-width: 200vh;
+table {
+  border-collapse: separate;
+  border-spacing: 100px 0;
   position: relative;
   left: 50%;
   transform: translateX(-50%);
   padding-top: 4vh;
 }
 
-
 #profile_view {
   background-image: linear-gradient(to bottom right, #3eadcf, #abe9cd);
   padding-bottom: 50vh;
+}
+
+.btn {
+  border-radius: 60px;
+  border: None;
+  width: 150px;
+  height: 55px;
+  color: #FDEDEC;
+  background-color: #45B39D ;
+}
+
+.buttonFrame {
+  padding-top: 10%;
+}
+
+tr {
+  width: 40px;
 }
 
 </style>
