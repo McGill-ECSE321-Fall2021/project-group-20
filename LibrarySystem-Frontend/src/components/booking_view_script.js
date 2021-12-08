0b1031b207c71a3c6212d3ace4d04642f4ac3066
AlexandruB30 <73802110+AlexandruB30@users.noreply.github.com>
2021-12-08 15:55:29 -0500

    Booking Fix

--- LibrarySystem-Frontend/src/components/booking_view_script.js
+++ LibrarySystem-Frontend/src/components/booking_view_script.js
@@ -29,29 +29,42 @@ export default {
       }).catch(msg => {
         console.log(msg.response.data)
         console.log(msg.response.status)
-        this.error = msg.response.data;
+        this.uperror = msg.response.data;
       })
 
     },
    // await axios.post("http://localhost:8080/booking/create?startDate=12/12/2021&endDate=12/25/2021&type=Reservation&barcode=" + iid + "&LibraryId=" + cid);
     Book(sdate,edate,Reservation,id){
-
       if (this.items.status !== "Available") {
         this.uperror = "Cannot book an unavailable item!"
       }
       else {
         AXIOS.post(backendUrl + '/booking/create?startDate=' + sdate + '&endDate=' + edate + '&type=' + Reservation + '&barcode=' + id + '&LibraryId=' + this.myid).then(response => {
           this.uperror = ''
-          this.$router.push('/')
-        }).catch(msg => {
-          console.log(msg.response.data)
-          console.log(msg.response.status)
-          this.uperror = msg.response.data;
+          AXIOS.get(backendUrl + '/items/id?itemId=' + id).then(response => {
+            this.response = response.data
+            this.item = response.data
+            this.uperror = ''
+          }).catch(msg => {
+            console.log(msg.response.data)
+            console.log(msg.response.status)
+            this.uperror = msg.response.data;
+          })
+          if (this.item.status !== 'Available') this.uperror = 'Cannot book this item!'
+          else {
+            AXIOS.post(backendUrl + '/booking/create?startDate=' + sdate + '&endDate=' + edate + '&type=' + Reservation + '&barcode=' + id + '&LibraryId=' + this.myid).then(response => {
+              this.uperror = ''
+              this.error = this.uperror
+              this.$router.push('/')
+            }).catch(msg => {
+              console.log(msg.response.data)
+              console.log(msg.response.status)
+              this.uperror = msg.response.data;
+            })
+          }
         })
       }
     },
-
-
     back(){
       this.$router.push('/')
     },
@@ -67,6 +80,7 @@ export default {
       response: [],
       items:[],
       uperror: '',
+      item: [],
       heads:{
 
       },
